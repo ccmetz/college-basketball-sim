a13aee48cebe12ca865fd4bd77f631d985129a6b
Cody Metz <ccmetz92@gmail.com>
2016-07-28 17:10:24 -0500

    Add beginnings of a Shot class (Issue #20)

---
+++ app/src/main/java/CbbSimEngine/Shot.java
@@ -0,0 +1,15 @@
+package CbbSimEngine;
+
+/**
+ * Created by ccmetz on 7/28/16.
+ *
+ * This class will handle what happens when a player attempts to shoot the ball
+ *
+ * Will change modifiers based on the shot type and determine the result of the shot
+ */
+public class Shot {
+
+    public Shot(){
+        
+    }
+}
